0905fb3da07a12bf0f92ae5ecc51b9ab73b4a985
Xinnan <luoxinnan1@gmail.com>
2023-03-17 00:15:07 +0100

    Random display correct button, check users choice

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var mainColor = ["#007965", "#F58634", "#FFCC29", "#0F4C75", "#FF7B54", "#4ECCA3"];
+var correctColor = "red";
+var correctButton = "";
+var userChosenButton = "";
 
 var level = 0;
 
@@ -11,17 +14,41 @@ $(document).on("keydown", function(){
   }
 });
 
+// find user chosen color, log it to users choice, and animate it.
+$(".btn").click(function() {
+    userChosenButton = $(this).attr("id");
+    // play sound
+    // animate button
+    checkAnswer();
+  });
+
 function nextColor() {
     level++;
     $(".level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 6);
-    var randomChosenColour = mainColor[randomNumber];
-  
-    $(".btn").css("background-color", randomChosenColour);
+    var randomChosenColor = mainColor[randomNumber];
+    $(".btn").css("background-color", randomChosenColor);
+    correctButton = Math.floor(Math.random() * 36);
+    $("#" + correctButton).css("background-color", correctColor);
+    
     // playSound
   }
 
+function checkAnswer(){
+    if(userChosenButton == correctButton){
+        alert("right");
+    }else {
+        // play sound
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press a Key to Restart");
+        // display the level and record.
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
   
+        // start over
+      }
+}
 
 
 
